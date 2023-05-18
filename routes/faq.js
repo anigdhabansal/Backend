fb79acf5b0f7a67af01733a09488811fdfdcb89b
anigdhabansal <Anigdha.Bansal@genpact.com>
2023-05-18 17:48:22 +0530

    final-faq

--- routes/faq.js
+++ routes/faq.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router()
-const fetchadmin = require('../middleware/fetchadmin');
 const Faq = require('../models/Faq')
 const {body, validationResult} = require('express-validator');
 
 
 // Route 1 get all the Faqs using :GET "api/Faqs/fetchallFaqs" Login required
 
-router.get('/fetchfaq', fetchadmin, async (req, res) => {
+router.get('/fetchfaq', async (req, res) => {
     try {
-        const faq = await Faq.find({admin: req.admin.id});
+        const faq = await Faq.find();
         res.json(faq)
 
     } catch (error) {
@@ -32,7 +31,7 @@ router.get('/fetchallfaq',  async (req, res) => {
 })
 
 // Route 2 Add a new Faq using POST " api/Faqs/addFaq" .Login Required
-router.post('/addfaq',fetchadmin, [
+router.post('/addfaq', [
     body('question', 'Title mut be 3 character long').isLength(
         {min: 3}
     ),
@@ -47,7 +46,7 @@ router.post('/addfaq',fetchadmin, [
         if (! errors.isEmpty()) {
             return res.status(400).json({errors: errors.array()});
         }
-        const faq = new Faq({question, answer, tag, admin: req.admin.id})
+        const faq = new Faq({question, answer, tag})
         const savedFaq = await faq.save()
 
         res.json({message: "Faq is added."})
